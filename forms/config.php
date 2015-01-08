40fdee6fce8f631e2e85305ea1fe8a1bac2c33a0
careck <careck@gmail.com>
2015-01-09 10:37:38 +1100

    updated from current cmfive master

--- forms/config.php
+++ forms/config.php
@@ -1,7 +1,8 @@
 <?php
 
-$modules['forms'] = array(
-    'active' => true,
+Config::set('forms', array(
+    'version' => '0.8.0',
+    'active' => false,
     'path' => 'system/modules',
     'topmenu' => true,
-);
+));
